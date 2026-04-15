92ec71ed771949bd3e29cb97116f6e95e0134e5d
amaryange <melessangekevin@gmail.com>
2026-04-15 17:02:10 +0000

    feat: remplace le logo Viho par /assets/images/logo.png

--- src/Component/Header/LeftHeader/index.tsx
+++ src/Component/Header/LeftHeader/index.tsx
@@ -11,7 +11,7 @@ const LeftHeader = () => {
     <div className='main-header-left'>
       <div className={logoToggle ? 'dark-logo-wrapper' : 'logo-wrapper'}>
         <a href={Javascript}>
-          <Image alt='logo' width={83} height={29} className='img-fluid' src={`${AssetsImagePath}/logo/${logoToggle ? 'dark-logo.png' : 'logo.png'}`} unoptimized />
+          <Image alt='logo' width={83} height={29} className='img-fluid' src={`${AssetsImagePath}/logo.png`} unoptimized />
         </a>
       </div>
       <div className='toggle-sidebar' onClick={toggleSidebar}>
